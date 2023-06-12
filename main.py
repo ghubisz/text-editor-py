49e03ba0a96be8fbdb1b7678a10b21439d8ad1ad
ghubisz <gab.op.pl@op.pl>
2023-06-12 14:24:35 +0200

    notepad

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+#create root window
+root = Tk()
+
+#design
+root.geometry("300x300")
+root.minsize(height=560)
+root.title("Text Editor")
+
+#widgets, buttons, etc
+root.mainloop()
