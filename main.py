10e8b58867f120e8eaa15935469ef76325ad9094
ghubisz <gab.op.pl@op.pl>
2023-06-12 14:29:09 +0200

    done

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ from tkinter import *
 root = Tk()
 
 #design
-root.geometry("300x300")
-root.minsize(height=560)
+root.geometry("350x250")
+root.minsize(height=560,
+             width=560)
+root.maxsize(height=250,
+             width=350)
 root.title("Text Editor")
+
+#scrollbar implementation
+scrollbar = Scrollbar(root)
+
+scrollbar.pack(side=RIGHT,
+               fill=Y)
+
+text_info = Text(root,
+                 yscrollcommand=scrollbar.set)
+text_info.pack(fill=BOTH)
+
+scrollbar.config(command=text_info.yview)
 
 #widgets, buttons, etc
 root.mainloop()
